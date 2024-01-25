0028e407c22b84c85f360e2e055654b4b1729a87
JUYEON LEE <dlwndus35@yonsei.ac.kr>
2024-01-25 22:07:20 +0900

    update 24/01/25

---
+++ 백준/baekjoon/src/main/java/baekjoon/자료구조/투포인터/P1253_좋다.java
@@ -0,0 +1,48 @@
+package baekjoon.자료구조.투포인터;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.*;
+
+public class P1253_좋다 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st;
+        int N = Integer.parseInt(br.readLine());
+
+        int answer = 0;
+
+        long[] arr = new long[N];
+
+        st = new StringTokenizer(br.readLine());
+        for (int i = 0; i < arr.length; i++) {
+            arr[i] = Integer.parseInt(st.nextToken());
+        }
+
+        Arrays.sort(arr);
+
+        for (int i = 0; i < arr.length; i++) {
+            long goodNum = arr[i];
+
+            int s = 0;
+            int e = arr.length-1;
+
+            while (s < e) {
+                if (arr[s] + arr[e] == goodNum) {
+                    //음수값도 고려해야 하므로
+                    if (s != i && e != i) {
+                        answer++;
+                        break;
+                    }
+                    if (s == i) s++;
+                    if (e == i) e--;
+                }else if (arr[s] + arr[e] < goodNum) s++;
+                else if (arr[s] + arr[e] > goodNum) e--;
+            }
+        }
+
+        System.out.println(answer);
+
+    }
+}
