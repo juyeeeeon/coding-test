47dd6708b7da8bfab30bf829ba0e4add42313a21
JUYEON LEE <dlwndus35@yonsei.ac.kr>
2024-02-20 20:25:10 +0900

    Add: 24.02.20 과제 추가

--- codingTest/src/main/java/baekjoon/탐색/P15686_치킨배달.java
+++ codingTest/src/main/java/baekjoon/탐색/P15686_치킨배달.java
@@ -5,16 +5,15 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.*;
 
+/**
+ * M개의 치킨집 조합 => 집과 치킨집의 거리의 최소값 => 합
+ */
 public class P15686_치킨배달 {
-    static final int CHICKEN_SPOT = 3;
     static int N, M;
-    static int minChickenStreetSum = Integer.MAX_VALUE;
-    static int chickenStreet = 0;
-    static int chickenStreetSum = 0; //치킨거리합
-    static int[][] map, testMap;
-    static int[][] deltas = {{-1, 0}, {1, 0}, {0, -1}, {0, 1}}; //상 하 좌 우
-    static boolean[][] visited;
-    static ArrayList<int[]> chickensLocation, selectChickens, homeLocation;
+    static int minChickenStreetSum = Integer.MAX_VALUE; // 최소치킨거리합
+    static int chickenStreet, chickenStreetSum; //치킨거리, 치킨거리합
+    static int[][] map;
+    static ArrayList<int[]> chickenLocation, selectChickens, homeLocation; // 치킨집위치, M개의 치킨집위치, 집위치
 
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
@@ -24,21 +23,19 @@ public class P15686_치킨배달 {
         M = Integer.parseInt(st.nextToken());
 
         map = new int[N + 1][N + 1];
-        testMap = new int[N + 1][N + 1];
-        visited = new boolean[N + 1][N + 1];
-        chickensLocation = new ArrayList<>(); //치킨집 위치 저장
+        chickenLocation = new ArrayList<>(); //치킨집 위치 저장
+        selectChickens = new ArrayList<>(); //M개의 치킨집을 담는 배열
         homeLocation = new ArrayList<>(); //집 위치 저장
 
         for (int r = 1; r <= N; r++) {
             st = new StringTokenizer(br.readLine());
             for (int c = 1; c <= N; c++) {
                 map[r][c] = Integer.parseInt(st.nextToken());
-                if (map[r][c] == 2) chickensLocation.add(new int[]{r, c}); //치킨집이면 치킨집 좌표 저장
                 if (map[r][c] == 1) homeLocation.add(new int[]{r, c}); //집이면 집 좌표 저장
+                if (map[r][c] == 2) chickenLocation.add(new int[]{r, c}); //치킨집이면 치킨집 좌표 저장
             }
         }
 
-        selectChickens = new ArrayList<>(); //M개의 치킨집을 담는 배열
         //M개의 치킨집을 고르는 모든 조합
         comb(0, 0);
 
@@ -54,73 +51,31 @@ public class P15686_치킨배달 {
      */
     private static void comb(int cnt, int start) {
         if (cnt == M) { //치킨집 M개가 골라지면
-            setTestMap(); //testMap에서 골라진 치킨집을 CHICKEN_SPOT으로 업데이트
-            startAtHome(); //집에서부터 짧은 치킨거리 찾기
+            findShortestChickenStreet(); //집에서부터 짧은 치킨거리 찾기
             return;
         }
 
-        for (int i = start; i < chickensLocation.size(); i++) {
-            selectChickens.add(chickensLocation.get(i));
+        for (int i = start; i < chickenLocation.size(); i++) {
+            selectChickens.add(chickenLocation.get(i));
             comb(cnt + 1, i + 1);
             selectChickens.remove(selectChickens.size() - 1); //원복
         }
     }
 
-    private static void setTestMap() {
-        for (int i = 1; i <= N; i++) {
-            for (int j = 1; j <= N; j++) {
-                testMap[i][j] = map[i][j];
-            }
-        }
-        for (int[] selectChicken : selectChickens) {
-            testMap[selectChicken[0]][selectChicken[1]] = CHICKEN_SPOT;
-        }
-    }
 
-    private static void startAtHome() {
+    private static void findShortestChickenStreet() {
         chickenStreetSum = 0; //치킨거리합 초기화
-
-        for (int[] home : homeLocation) {
+        for (int h = 0; h < homeLocation.size(); h++) { //하나의 집마다
             chickenStreet = Integer.MAX_VALUE; //치킨거리 초기화
-            dfs(home[0], home[1], home[0], home[1]);
+            for (int c = 0; c < selectChickens.size(); c++) { //치킨집까지의 거리
+                int[] curHome = homeLocation.get(h);
+                int[] curChick = selectChickens.get(c);
+                int distance = getDistance(curHome[0], curHome[1], curChick[0], curChick[1]);
+                chickenStreet = Math.min(chickenStreet, distance); //치킨거리의 최솟값
+            }
             chickenStreetSum += chickenStreet;
         }
-
         minChickenStreetSum = Math.min(minChickenStreetSum, chickenStreetSum); //최소 치킨거리합 초기화
-    }
-
-    /**
-     *
-     * @param r 탐색할 행
-     * @param c 탐색할 열
-     * @param hr 집 열
-     * @param hc 집 행
-     */
-    private static void dfs(int r, int c, int hr, int hc) {
-        if (visited[r][c]) return;
-
-        //치킨집이면
-        if (testMap[r][c] == CHICKEN_SPOT) {
-            int distance = getDistance(hr, hc, r, c);
-            chickenStreet = Math.min(chickenStreet, distance); //치킨거리 최솟값으로 업데이트
-            return;
-        }
-
-        visited[r][c] = true;
-
-        for (int d = 0; d < deltas.length; d++) {
-            int nr = r + deltas[d][0];
-            int nc = c + deltas[d][1];
-
-            if (isValid(nr, nc) && !visited[nr][nc])
-                dfs(nr, nc, hr, hc);
-        }
-
-        visited[r][c] = false; //다른 탐색을 위해 원복
-    }
-
-    private static boolean isValid(int nr, int nc) {
-        return nr > 0 && nc > 0 && nr <= N && nc <= N;
     }
 
     private static int getDistance(int r1, int c1, int r2, int c2){
